4abaff32abe21badddb84ffa2fbb4a996fb596c7
Kristine <screamingmunch@gmail.com>
2014-01-25 11:46:30 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodeCorps::Application.config.secret_token = '9c5abe0494d089b69577a54762d7acbe4b11061a2cac3f516473f481baa6fdbf2fb314e3a2eed6d09649b316d2c58cc0e9a04f0694708068ecb1d9a045ea8a66'
